a7438d84d80f20f8f30eef66e96d1d02da83af92
Kirill Gantimurov <gantimkir@ya.ru>
2016-08-08 15:30:36 +0300

    2nd

--- app/src/main/java/com/example/kirill/p4/ActivityThree.java
+++ app/src/main/java/com/example/kirill/p4/ActivityThree.java
@@ -3,9 +3,13 @@ package com.example.kirill.p4;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityThree extends AppCompatActivity {
     public final static String READ_CODE = "com.example.kirill.p4.RES_CODE";
@@ -32,7 +36,20 @@ public class ActivityThree extends AppCompatActivity {
                     setResult(RESULT_CANCELED,intent);
             }
                 finish();
+            case R.id.buttonAdvancedToast:
+                //создаем и отображаем текстовое уведомление
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Пора покормить кота!",
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                LinearLayout toastContainer = (LinearLayout) toast.getView();
+                ImageView catImageView = new ImageView(getApplicationContext());
+                catImageView.setImageResource(R.drawable.cat_bottom);
+                toastContainer.addView(catImageView, 0);
+                toast.show();
+                break;
         }
 
     }
+
 }
