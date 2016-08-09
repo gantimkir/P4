987a63a1809f76c38c33c98b5c03fd13bf164e24
Kirill Gantimurov <gantimkir@ya.ru>
2016-08-09 17:25:22 +0300

    9th

--- app/src/main/java/com/example/kirill/p4/ActivityFive.java
+++ app/src/main/java/com/example/kirill/p4/ActivityFive.java
@@ -1,14 +1,17 @@
 package com.example.kirill.p4;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityFive extends AppCompatActivity {
-
+    Snackbar snackbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +23,43 @@ public class ActivityFive extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "!!!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                snackbar = Snackbar.make(view, "!!!", Snackbar.LENGTH_LONG).setAction("Action", snackbarOnClickListener);
+                snackbar.setDuration(8000);
+                View snackbarView=snackbar.getView();
+                snackbar.setActionTextColor(Color.GREEN); // цвет текста у кнопки действия
+                snackbarView.setBackgroundColor(Color.BLACK);
+                TextView snackTextView=(TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                snackTextView.setTextColor(Color.RED);
+                snackbar.setCallback(new Snackbar.Callback() {
+                    @Override
+                    public void onDismissed(Snackbar snackbar, int event) {
+                        // do some action on dismiss
+                        Toast.makeText(getApplicationContext(), "My dismiss", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onShown(Snackbar snackbar) {
+                        // do some action when snackbar is showed
+                        Toast.makeText(getApplicationContext(), "Snackbar is showed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                snackbar.show();
             }
         });
     }
 
+    View.OnClickListener snackbarOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(getApplicationContext(), "Молодец!", Toast.LENGTH_LONG).show();
+        }
+    };
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonDismiss:
+                snackbar.dismiss();
+                break;
+        }
+    }
 }
