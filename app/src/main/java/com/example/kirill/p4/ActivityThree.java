738bf48b41cf6d1f2e95b8458a9292ae3ddddc71
Kirill Gantimurov _ <gantimkir@ya.ru>
2016-08-10 14:40:31 +0300

    10th

--- app/src/main/java/com/example/kirill/p4/ActivityThree.java
+++ app/src/main/java/com/example/kirill/p4/ActivityThree.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
@@ -20,6 +22,41 @@ public class ActivityThree extends AppCompatActivity {
         String MessageFromActivityOne=getIntent().getExtras().getString("MessageFromActivityOne");
         TextView textView_activity3_tv = (TextView)findViewById(R.id.textView_activity3_tv);
         textView_activity3_tv.setText(MessageFromActivityOne);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        return super.onOptionsItemSelected(item);
+        // получим идентификатор выбранного пункта меню
+        int id = item.getItemId();
+        TextView infoTextView = (TextView) findViewById(R.id.textviewMenuExplorer);
+        // Операции для выбранного пункта меню
+        switch (id) {
+            case R.id.action_cat1:
+                infoTextView.setText("Вы выбрали кота!");
+                return true;
+            case R.id.action_cat2:
+                infoTextView.setText("Вы выбрали кошку!");
+                return true;
+            case R.id.action_cat3:
+                infoTextView.setText("Вы выбрали котёнка!");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // у атрибута пункта меню Settings установлено значение android:onClick="onSettingsMenuClick"
+    public void onSettingsMenuClick(MenuItem item) {
+        TextView infoTextView = (TextView) findViewById(R.id.textviewMenuExplorer);
+        infoTextView.setText("Вы выбрали пункт Settings, лучше бы выбрали кота");
     }
 
     public void onClick(View view) {
